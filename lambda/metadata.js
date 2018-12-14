91235772e7794fd4d5b2f206ac58b4a9a9da7ca8
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-12-14 11:01:26 +0100

    Step 6: Add opensea

--- lambda/metadata.js
+++ lambda/metadata.js
@@ -1,12 +1,48 @@
 exports.handler = function(event, context, callback) {
   const tokenId = event.queryStringParameters.tokenId
   const metadata =  {
-      "name": "Token #" + tokenId,
-      "description": "Describes the asset to which this token represents",
-      "image": "https://dummyimage.com/600x400/000000/fff/&text=token%20" + tokenId,
-  }
+    "name": "Token #" + tokenId,
+    "external_url": "https://block-workshop.netlify.com/", 
+    "description": "This is a very basic NFT with token Id #" + tokenId,
+    "image": "https://dummyimage.com/600x400/000000/fff/&text=token%20" + tokenId,
+    "attributes": [
+      {
+        "trait_type": "zodiac", 
+        "value": returnZodiac(tokenId)
+      }
+    ]
+  } 
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(metadata)
   });
 };
+function returnZodiac(tokenId) {
+  const month = ((tokenId - 1) % 12) + 1
+  switch(month) {
+    case(1):
+      return 'Capricorn'
+    case(2):
+      return 'Aquarius'
+    case(3):
+      return 'Pisces'
+    case(4):
+      return 'Aries'
+    case(5):
+      return 'Taurus'
+    case(6):
+      return 'Gemini'
+    case(7):
+      return 'Cancer'
+    case(8):
+      return 'Leo'
+    case(9):
+      return 'Virgo'
+    case(10):
+      return 'Libra'
+    case(11):
+      return 'Scorpio'
+    case(12):
+      return 'Sagittarius'
+  }
+}
