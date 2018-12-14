0cb291a0133ddadd6023b7f796d118415f7ef565
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-12-14 10:23:57 +0100

    Step 5: Create Migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,21 @@
-var Sample = artifacts.require('./Sample.sol')
+var Metadata = artifacts.require('./Metadata.sol')
+var Token = artifacts.require('./Token.sol')
+
 let _ = '        '
 
 module.exports = (deployer, helper, accounts) => {
 
   deployer.then(async () => {
     try {
-      // Deploy Sample.sol
-      await deployer.deploy(Sample)
-      let sample = await Sample.deployed()
-      console.log(_ + 'Sample deployed at: ' + sample.address)
+      // Deploy Metadata.sol
+      await deployer.deploy(Metadata)
+      let metadata = await Metadata.deployed()
+      console.log(_ + 'Metadata deployed at: ' + metadata.address)
+
+     // Deploy Token.sol
+      await deployer.deploy(Token, 'Token Name', 'Token Symbol', metadata.address)
+      let token = await Token.deployed()
+      console.log(_ + 'Token deployed at: ' + token.address)
 
     } catch (error) {
       console.log(error)
