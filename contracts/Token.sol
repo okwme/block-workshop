6412b87248e2daa6de2f93e3836a9a5db1449285
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-12-14 10:20:08 +0100

    Step 4: Add Metadata to ERC-721

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,11 +1,31 @@
 pragma solidity ^0.4.20;
 import "zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
+import './Metadata.sol';
 
 /**
  * The Token contract does this and that...
  */
 contract Token is ERC721Token {
-    constructor(string name, string symbol) public
+    address metadata;
+    constructor(string name, string symbol, address _metadata) public
         ERC721Token(name, symbol)
-    { }
+    { 
+        metadata = _metadata;
+    }
+    function getMetadata() public view returns (address) {
+        return metadata;
+    }
+    function tokenURI(uint _tokenId) public view returns (string _infoUrl) {
+        address _impl = getMetadata();
+        bytes memory data = msg.data;
+        assembly {
+            let result := delegatecall(gas, _impl, add(data, 0x20), mload(data), 0, 0)
+            let size := returndatasize
+            let ptr := mload(0x40)
+            returndatacopy(ptr, 0, size)
+            switch result
+            case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
+    }
 }
